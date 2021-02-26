68f638be4e944e54d656d4706b247654d0bd01e4
Evan LaBate <49501559+JonGrayson@users.noreply.github.com>
2021-02-25 23:46:39 -0700

    Add files via upload

---
+++ Scripts/Ballistics.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Ballistics : MonoBehaviour
+{
+    public float myTimeScale = 1.0f;
+    public GameObject target;
+    public float launchForce = 10f;
+    Rigidbody rb;
+    Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        Time.timeScale = myTimeScale;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            FiringSolution firingSolution = new FiringSolution();
+            Nullable<Vector3> aimVector = firingSolution.Calculate(transform.position, target.transform.position, launchForce, Physics.gravity);
+            if(aimVector.HasValue)
+            {
+                rb.AddForce(aimVector.Value.normalized * launchForce, ForceMode.VelocityChange);
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            rb.isKinematic = true;
+            transform.position = startPosition;
+            rb.isKinematic = false;
+        }
+    }
+}
